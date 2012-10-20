58a48fe21e1d554b9cd12d13b039ee803d6eec98
Daniel Jabbour <daniel@drjweb.com>
2012-10-20 00:29:22 -0700

    Adding favicon generation, minify css / js

--- Gemfile
+++ Gemfile
@@ -1,3 +1,9 @@
 source :rubygems
 
 gem "middleman", "~>3.0.5"
+
+# Compresses PNGs
+gem "middleman-smusher", "~> 3.0.0"
+
+# Make a favicon from one big base image
+gem "middleman-favicon-maker", "~> 3.0.0"
--- config.rb
+++ config.rb
@@ -59,10 +59,10 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :cache_buster
@@ -72,9 +72,15 @@ configure :build do
 
   # Compress PNGs after build
   # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
+  require "middleman-smusher"
+  activate :smusher
 
   # Or use a different image path
   # set :http_path, "/Content/images/"
+
+  # Consider one day to gzip and serve with gzip_static on nginx
+  # activate :gzip
+
+  # Generate favicons of various sizes from favicon_base.png (114px sq)
+  activate :favicon_maker
 end
