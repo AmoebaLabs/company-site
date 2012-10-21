f7e3ef54089ec432471a7105f6e0582abce78bb0
Daniel Jabbour <daniel@drjweb.com>
2012-10-21 16:37:21 -0700

    Changing normalize.css over to an in-project stylesheet in vendor/. Was having trouble with the compass gem.

--- Gemfile
+++ Gemfile
@@ -7,6 +7,3 @@ gem "middleman-smusher", "~> 3.0.0"
 
 # Make a favicon from one big base image
 gem "middleman-favicon-maker", "~> 3.0.0"
-
-# Normalize.css for better default cross-browser markup
-gem "compass-normalize", "~> 1.4"
--- config.rb
+++ config.rb
@@ -6,9 +6,6 @@
 # First: gem install susy --pre
 # require 'susy'
 
-# Normalize.css for better default cross-browser business
-require 'compass-normalize'
-
 # Change Compass configuration
 # compass_config do |config|
 #   config.output_style = :compact
